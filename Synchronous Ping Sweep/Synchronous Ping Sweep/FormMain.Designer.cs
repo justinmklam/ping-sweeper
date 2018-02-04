50b8084a53e696c832905f9974c9c41afd8114bf
JUSTIN-X220\justi <justinmklam@gmail.com>
2018-02-03 17:57:55 -0800

    Added project files

---
+++ Synchronous Ping Sweep/Synchronous Ping Sweep/FormMain.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Synchronous_Ping_Sweep
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSweepSync = new System.Windows.Forms.Button();
+            this.buttonSweepAsync = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonSweepSync
+            // 
+            this.buttonSweepSync.Location = new System.Drawing.Point(59, 19);
+            this.buttonSweepSync.Name = "buttonSweepSync";
+            this.buttonSweepSync.Size = new System.Drawing.Size(134, 30);
+            this.buttonSweepSync.TabIndex = 0;
+            this.buttonSweepSync.Text = "Single Thread";
+            this.buttonSweepSync.UseVisualStyleBackColor = true;
+            this.buttonSweepSync.Click += new System.EventHandler(this.buttonPingSweep_Click);
+            // 
+            // buttonSweepAsync
+            // 
+            this.buttonSweepAsync.Location = new System.Drawing.Point(59, 71);
+            this.buttonSweepAsync.Name = "buttonSweepAsync";
+            this.buttonSweepAsync.Size = new System.Drawing.Size(134, 30);
+            this.buttonSweepAsync.TabIndex = 1;
+            this.buttonSweepAsync.Text = "Multithread";
+            this.buttonSweepAsync.UseVisualStyleBackColor = true;
+            this.buttonSweepAsync.Click += new System.EventHandler(this.buttonSweepAsync_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 123);
+            this.Controls.Add(this.buttonSweepAsync);
+            this.Controls.Add(this.buttonSweepSync);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping Sweeper";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSweepSync;
+        private System.Windows.Forms.Button buttonSweepAsync;
+    }
+}
+
---
+++ Synchronous Ping Sweep/Synchronous Ping Sweep/FormMain.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Synchronous_Ping_Sweep
+{
+    public partial class FormMain : Form
+    {
+        PingSweep pingSweep = new PingSweep();
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPingSweep_Click(object sender, EventArgs e)
+        {
+            buttonSweepSync.Enabled = false;
+            pingSweep.RunPingSweep_Sync();
+            buttonSweepSync.Enabled = true;
+        }
+
+        private void buttonSweepAsync_Click(object sender, EventArgs e)
+        {
+            buttonSweepSync.Enabled = false;
+            pingSweep.RunPingSweep_Async();
+            buttonSweepSync.Enabled = true;
+        }
+    }
+
+    public class PingSweep
+    {
+        private string BaseIP = "192.168.1.";
+        private int StartIP = 1;
+        private int StopIP = 255;
+        private string ip;
+
+        private int timeout = 100;
+        private int nFound = 0;
+
+        static object lockObj = new object();
+        Stopwatch stopWatch = new Stopwatch();
+        TimeSpan ts;
+
+        public void RunPingSweep_Sync()
+        {
+            nFound = 0;
+
+            stopWatch.Start();
+            System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+
+            for (int i = StartIP; i <= StopIP; i++)
+            {
+                ip = BaseIP + i.ToString();
+                System.Net.NetworkInformation.PingReply rep = p.Send(ip, timeout);
+
+                if (rep.Status == System.Net.NetworkInformation.IPStatus.Success)
+                {
+                    nFound++;
+                }
+            }
+
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+
+            MessageBox.Show(nFound.ToString() + " devices found! Elapsed time: " + ts.ToString(), "Single Threaded");
+        }
+
+        public async void RunPingSweep_Async()
+        {
+            nFound = 0;
+
+            var tasks = new List<Task>();
+
+            stopWatch.Start();
+
+            for (int i = StartIP; i <= StopIP; i++)
+            {
+                ip = BaseIP + i.ToString();
+
+                System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+                var task = PingAndUpdateAsync(p, ip);
+                tasks.Add(task);
+            }
+
+            await Task.WhenAll(tasks);
+
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+            MessageBox.Show(nFound.ToString() + " devices found! Elapsed time: " + ts.ToString(), "Multithreaded");
+        }
+
+        private async Task PingAndUpdateAsync(System.Net.NetworkInformation.Ping ping, string ip)
+        {
+            var reply = await ping.SendPingAsync(ip, timeout);
+
+            if (reply.Status == System.Net.NetworkInformation.IPStatus.Success)
+            {
+                lock(lockObj)
+                {
+                    nFound++;
+                }
+            }
+
+        }
+    }
+}
